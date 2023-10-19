67b8a0166171e82f28bba252e81370981d46003b
Hannah Shafferman <hannahshafferman@Hannahs-MacBook-Pro.local>
2023-10-19 19:20:22 -0400

    Adding library import

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-import subprocess
-import os
-import sys
-sys.path.append('pddl-parser')
-sys.path.append('pddl-parser/pddl_parser')
-from PDDL import PDDL_Parser
+from pddl_parser_root.pddl_parser.planner import Planner
+from pddl_parser_root.pddl_parser.PDDL import PDDL_Parser
 
 def main():
-    parser = PDDL_Parser
-    #os.chdir(os.path.join(os.path.abspath(os.path.curdir), u'pddl-parser'))
-    #os.system('python3 -B -m pddl_parser.PDDL ../blockworld.pddl ../pb1.pddl')
+    parser = PDDL_Parser()
+    print(parser)
 
 if __name__ == "__main__":
     main()
