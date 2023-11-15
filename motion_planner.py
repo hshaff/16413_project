9bbcacb06801c34b313cda5a546acbd1fab344e4
hshaff <hshaff@github.com>
2023-11-15 17:39:06 -0500

    Fixed paths to run from this repo

--- motion_planner.py
+++ motion_planner.py
@@ -5,7 +5,7 @@ import sys
 import argparse
 import numpy as np
 
-sys.path.extend(os.path.abspath(os.path.join(os.getcwd(), d)) for d in ['pddlstream', 'ss-pybullet'])
+sys.path.extend(os.path.abspath(os.path.join(os.getcwd(), d)) for d in ['padm_project', 'padm_project/ss-pybullet'])
 
 from pybullet_tools.utils import set_pose, Pose, Point, Euler, multiply, get_pose, get_point, create_box, set_all_static, WorldSaver, create_plane, COLOR_FROM_NAME, stable_z_on_aabb, pairwise_collision, elapsed_time, get_aabb_extent, get_aabb, create_cylinder, set_point, get_function_name, wait_for_user, dump_world, set_random_seed, set_numpy_seed, get_random_seed, get_numpy_seed, set_camera, set_camera_pose, link_from_name, get_movable_joints, get_joint_name
 from pybullet_tools.utils import CIRCULAR_LIMITS, get_custom_limits, set_joint_positions, interval_generator, get_link_pose, interpolate_poses
@@ -13,8 +13,8 @@ from pybullet_tools.utils import CIRCULAR_LIMITS, get_custom_limits, set_joint_p
 from pybullet_tools.ikfast.franka_panda.ik import PANDA_INFO, FRANKA_URDF
 from pybullet_tools.ikfast.ikfast import get_ik_joints, closest_inverse_kinematics
 
-from src.world import World
-from src.utils import JOINT_TEMPLATE, BLOCK_SIZES, BLOCK_COLORS, COUNTERS, \
+from padm_project.src.world import World
+from padm_project.src.utils import JOINT_TEMPLATE, BLOCK_SIZES, BLOCK_COLORS, COUNTERS, \
     ALL_JOINTS, LEFT_CAMERA, CAMERA_MATRIX, CAMERA_POSES, CAMERAS, compute_surface_aabb, \
     BLOCK_TEMPLATE, name_from_type, GRASP_TYPES, SIDE_GRASP, joint_from_name, \
     STOVES, TOP_GRASP, randomize, LEFT_DOOR, point_from_pose, translate_linearly
@@ -80,6 +80,7 @@ def main():
     print("Going to operate the base without collision checking")
     for i in range(100):
         goal_pos = translate_linearly(world, 0.01) # does not do any collision checking!!
+        #print(get_collision_data(world.robot))
         set_joint_positions(world.robot, world.base_joints, goal_pos)
         if (i % 30 == 0):
             wait_for_user()
